0b42bc87471178f033876c2e3e2edf36e1b8cbd5
Aazharul Kabir <135993951+azharulkabir@users.noreply.github.com>
2025-02-17 22:40:02 +0600

    Add files via upload

---
+++ class.js
@@ -0,0 +1,15 @@
+
+const input = document.querySelector("input");
+const p = document.querySelector("p");
+input.addEventListener("copy",function(){
+        p.innerText="you have copied";
+        // console.log("you have copied");
+});
+
+input.addEventListener("cut",function(){
+     console.log("you have cut");
+});
+
+input.addEventListener("paste",function(){
+    console.log("you have pasted");
+});
